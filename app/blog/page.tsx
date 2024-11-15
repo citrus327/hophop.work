1dd47da65464dacf6d2dc13104e4a82f0e95a711
citrus327 <citrus327@outlook.com>
2024-11-15 23:28:20 +0800

    feat: basic

---
+++ app/blog/page.tsx
@@ -0,0 +1,10 @@
+import Link from "next/link";
+
+export default function Blog() {
+  return (
+    <div>
+      <Link href="/blog/1">Blog 1</Link>
+      <Link href="/blog/2">Blog 2</Link>
+    </div>
+  );
+}
