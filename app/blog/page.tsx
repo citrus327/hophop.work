193d0f21c6dd2d662c24ad08aa86ce71ac360f34
citrus327 <citrus327@outlook.com>
2024-11-21 01:57:14 +0800

    feat: pages

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,12 +1,19 @@
 import Link from "next/link";
 
+interface Page {
+  pageId: string;
+  createdTime: string;
+  lastEditedTime: string;
+  title: string;
+}
+
 export default async function Blog() {
   const pages = await fetch(`https://workers.hophop.work/blog/pages`).then(
     (res) => res.json()
   );
   return (
     <div className="flex flex-col gap-2">
-      {pages.publishedPages.map((page, index) => (
+      {pages.publishedPages.map((page: Page, index: number) => (
         <div
           key={page.pageId}
           className="flex items-center gap-2 w-full hover:bg-gray-200 p-2 rounded-md cursor-pointer"
