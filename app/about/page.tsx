cbb09c61b4a04ba980c29391a4c656793a65248f
citrus327 <citrus327@outlook.com>
2024-11-22 00:26:12 +0800

    feat: blogs

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,6 +1,8 @@
 import { NotionAPI } from "notion-client";
 import { NotionPage } from "../components/notion-page";
 
+export const revalidate = 60;
+
 export default async function About() {
   const notion = new NotionAPI();
   const recordMap = await notion.getPage("b646be73efea4f8a95ed7d50343d34b1");
